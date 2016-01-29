90afb2562a11fca76b44e934d23a9879ae1fb6ba
tiann <923551233@qq.com>
2016-01-29 14:46:25 +0800

    directly copy file to upload complete

--- clipboard.py
+++ clipboard.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 import os
 import tempfile
+import imghdr
+import shutil
 
-from AppKit import NSPasteboard, NSPasteboardTypePNG, NSPasteboardTypeTIFF, NSPasteboardTypeString
+from AppKit import NSPasteboard, NSPasteboardTypePNG,\
+        NSPasteboardTypeTIFF, NSPasteboardTypeString,\
+        NSFilenamesPboardType
+
+NONE_IMG = (None, False)
+
+def _convert_to_png(from_path, to_path):
+    # convert it to png file
+    os.system('sips -s format png %s --out %s' % (from_path, to_path))
 
 def get_paste_img_file():
     ''' get a img file from clipboard;
@@ -19,29 +29,45 @@ def get_paste_img_file():
     png_file = tempfile.NamedTemporaryFile(suffix="png")
 
     supported_image_format = (NSPasteboardTypePNG, NSPasteboardTypeTIFF)
+    if NSFilenamesPboardType in data_type:
+        # file in clipboard
+        img_path = pb.propertyListForType_(NSFilenamesPboardType)[0]
+        img_type = imghdr.what(img_path)
+
+        if not img_type:
+            # not image file 
+            return NONE_IMG
+
+        if img_type not in ('png', 'jpeg'):
+            # now only support png & jpg
+            return NONE_IMG
+
+        tmp_clipboard_img_file = tempfile.NamedTemporaryFile()
+        shutil.copy(img_path, tmp_clipboard_img_file.name)
+        _convert_to_png(tmp_clipboard_img_file.name, png_file.name)
+        tmp_clipboard_img_file.close()
+        return png_file, False
+
     if NSPasteboardTypeString in data_type:
         # make this be first, because plain text may be TIFF format?
         # string todo, recognise url of png & jpg
         pass
-    elif any(filter(lambda f: f in data_type, supported_image_format)):
+
+    if any(filter(lambda f: f in data_type, supported_image_format)):
         # do not care which format it is, we convert it to png finally
         # system screen shotcut is png, QQ is tiff
         tmp_clipboard_img_file = tempfile.NamedTemporaryFile()
         print tmp_clipboard_img_file.name
-        data = pb.dataForType_(NSPasteboardTypePNG)
+        for fmt in supported_image_format:
+            data = pb.dataForType_(fmt)
+            if data: break
         ret = data.writeToFile_atomically_(tmp_clipboard_img_file.name, False)
-        if not ret: return
+        if not ret: return NONE_IMG
 
-        # convert it to png file
-        os.system('sips -s format png %s --out %s' % (tmp_clipboard_img_file.name, png_file.name))
-
+        _convert_to_png(tmp_clipboard_img_file.name, png_file.name)
         # close the file explicitly
         tmp_clipboard_img_file.close()
-        return png_file
+        return png_file, True
         
 if __name__ == '__main__':
     get_paste_img_file()
-
-
-
-
